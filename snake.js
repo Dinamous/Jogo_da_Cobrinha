ae32f4421f784e9ba10ed462c36ad7545c66689c
Matheus <matheusaugustosimoes@hotmail.com>
2020-01-14 12:47:22 -0300

    começado estrutura

---
+++ snake.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    var canvas = $('#canvas')[0]
+    var dimensao = canvas.getContext('2d')
+    var largura = $('#canvas').width()
+    var altura = $('#canvas').height()
+
+    var tamanho_corpo = 12
+    var anda
+    var comeu
+    var pontuacao
+    var corpo_cobra
+
+    
+})
